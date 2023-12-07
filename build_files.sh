d96e75cb50221736de795a38102b7b5f208beb03
Atia-Khan <atia.khan@xloopdigital.com>
2023-12-07 14:31:00 +0500

    ...

--- build_files.sh
+++ build_files.sh
@@ -11,6 +11,7 @@
 # python3 manage.py migrate 
 # python3 manage.py collectstatic
 
+
 #!/bin/bash
 # Install or upgrade pip
 # pip install --upgrade pip
@@ -21,10 +22,15 @@ python3 -m venv venv
 # Activate the virtual environment
 source venv/bin/activate
 # Install gettext (needed for msgfmt command)
+# Ensure that the system packages are up-to-date
 apt-get update
+# Install gettext
 apt-get install -y gettext
 # Install Python dependencies
 pip install --disable-pip-version-check --upgrade -r requirements.txt
+# Make sure the environment variables are set
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 # Make migrations
 python manage.py migrate
 # Collect static files
