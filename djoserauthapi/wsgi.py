af8fb15e0104b7d69a73a43fd84809238d5efe96
Atia-Khan <atia.khan@xloopdigital.com>
2023-12-07 12:58:47 +0500

    new

--- djoserauthapi/wsgi.py
+++ djoserauthapi/wsgi.py
@@ -11,8 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djoserauthapi.settings')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saveplate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djoserauthapi.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saveplate.settings')
 
 
 application = get_wsgi_application()
